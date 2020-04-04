baadccd21171c51f42cc90a71cb4ca05f9bde822
Limhes <49906101+Limhes@users.noreply.github.com>
2020-04-04 18:41:12 +1100

    Delete compile_wasm.sh

--- webassembly/compile_wasm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-em++ ./main.cpp ../source/system.cpp ../source/electrodes.cpp ../source/experiment.cpp ../source/coefs_alpha_beta.cpp ../source/simulation.cpp -o ../build/libwasmecsim.js -O3 -std=c++11 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']"
